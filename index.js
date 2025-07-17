62ca221b7c1b872b4afd43a1656be014a11112c2
Joaquin Requejo <joaquinrequejo18@gmail.com>
2025-07-17 12:34:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ name: 'Juan' })
+})
+
+app.listen(3000, () => {
+  console.log('App listening on http://localhost:3000')
+})
