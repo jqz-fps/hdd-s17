2013a6384bb5b5e9205218a5723eba4afcc58e49
Joaquin Requejo <joaquinrequejo18@gmail.com>
2025-07-17 13:01:12 -0500

    xd

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.get('/user', (req, res) => {
   res.json({ name: 'Juan', age: 30 })
 })
 
-app.get('/user/:id', (req, res) => {
-  res.json({ name: 'Juan', age: 30, id: req.params.id })
-})
-
 app.listen(3000, () => {
   console.log('App listening on http://localhost:3000')
 })
